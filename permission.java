d5dd6934f3ddfc9b3e74665322925e576397fb12
Ashraf4747 <Ashraf.mohamed4728@gmail.com>
2017-06-13 07:54:52 +0200

    Update permission.java

--- permission.java
+++ permission.java
@@ -6,41 +6,78 @@
 
 */
 //access to permsions
-    void CheckUserPermsions(){
-      if ( Build.VERSION.SDK_INT >= 23){
-                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
-                            PackageManager.PERMISSION_GRANTED  ){
-                     requestPermissions(new String[]{
-                                        android.Manifest.permission.ACCESS_FINE_LOCATION},
-                                REQUEST_CODE_ASK_PERMISSIONS);
-                        return ;
-                    }
-                }
+ void CheckUserPermsions()
 
-                getLastLocation();// init the contact list
-              
+    {
+        if (Build.VERSION.SDK_INT >= 23)
+        {
+            if ((ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+
+                    != PackageManager.PERMISSION_GRANTED)
+
+                        &&
+
+                    (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_CONTACTS)
+                    != PackageManager.PERMISSION_GRANTED))
+            {
+                if ((!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION))
+                        &&
+                   (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)))
+                {
+
+
+                    requestPermissions(new String[]
+                     {
+                      android.Manifest.permission.ACCESS_FINE_LOCATION,
+                      android.Manifest.permission.READ_CONTACTS
+                     }, REQUEST_CODE_ASK_PERMISSIONS);
+                    return;
+                }
+            }
+        }
+
+        GetLocation();// init the contact list
+
     }
+
     //get acces to location permsion
     final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
 
-
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        switch (requestCode)
+        {
             case REQUEST_CODE_ASK_PERMISSIONS:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getLastLocation();// init the contact list
-                } else {
-                    // Permission Denied
-                    Toast.makeText( this,"your message" , Toast.LENGTH_SHORT)
-                            .show();
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    GetLocation();// init the contact list
                 }
+
+                else if (grantResults[1] == PackageManager.PERMISSION_GRANTED)
+                {
+
+
+
+                }
+
+                else
+
+                    {
+                    // Permission Denied
+                    Toast.makeText(this, "you Denied the Permission", Toast.LENGTH_SHORT).show();
+                    }
+
                 break;
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+                default:
+
+                    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
+
 
 //Get last location
   public  void getLastLocation(){
